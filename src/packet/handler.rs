d7eae122bf79204ee992d9d1d7e02dbf2ba800ef
Maciej Gawrysiak <gawrysiak.maciej@proton.me>
2025-06-15 15:54:05 +0200

    feat(TCP): use ReassemblyManager for TCP reconstruction

--- src/packet/handler.rs
+++ src/packet/handler.rs
@@ -1,8 +1,21 @@
-use crate::{filter_engine::Verdict, packet::PacketMeta};
+use crate::{
+    filter_engine::Verdict,
+    packet::PacketMeta,
+    protocol::reassembly::{buffer::ReassemblyBuffer, manager::ReassemblyManager},
+};
+use std::hash::Hash;
 
 /// A packet‐inspector that can make accept/drop decisions
 pub trait PacketHandler {
     type Header;
 
     fn inspect(&mut self, meta: &PacketMeta, header: &Self::Header, payload: &[u8]) -> Verdict;
+}
+
+pub struct ReassemblyPacketHandler<K, B>
+where
+    K: Eq + Hash + Clone,
+    B: ReassemblyBuffer<Seq = u32>,
+{
+    reassembly: ReassemblyManager<K, B>,
 }
--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -1,5 +1,6 @@
 pub mod header;
 pub mod http;
 pub mod icmp;
+pub mod reassembly;
 pub mod tcp;
 pub mod udp;
